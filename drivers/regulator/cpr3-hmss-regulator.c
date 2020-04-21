ddcdb727cfb431bf0a01a5a6728c97a76c02afac
cc1286 <cc1286840533@gmail.com>
2020-04-21 10:39:55 +0800

    修改：     drivers/regulator/cpr3-hmss-regulator.c

--- drivers/regulator/cpr3-hmss-regulator.c
+++ drivers/regulator/cpr3-hmss-regulator.c
@@ -374,11 +374,11 @@ static const int msm8996_v1_v2_hmss_fuse_ref_volt[MSM8996_HMSS_FUSE_CORNERS] = {
 
 /* Open loop voltage fuse reference voltages in microvolts for MSM8996 v3 */
 static const int msm8996_v3_hmss_fuse_ref_volt[MSM8996_HMSS_FUSE_CORNERS] = {
-	605000,
-	745000, /* Place holder entry for LowSVS */
-	745000,
-	905000,
-	1140000,
+	600000,
+	735000, /* Place holder entry for LowSVS */
+	735000,
+	900000,
+	1100000,
 };
 
 /*
@@ -387,11 +387,11 @@ static const int msm8996_v3_hmss_fuse_ref_volt[MSM8996_HMSS_FUSE_CORNERS] = {
  */
 static const int msm8996_v3_speed_bin1_rev5_hmss_fuse_ref_volt[
 						MSM8996_HMSS_FUSE_CORNERS] = {
-	605000,
-	745000, /* Place holder entry for LowSVS */
-	745000,
-	905000,
-	1140000,
+	600000,
+	735000, /* Place holder entry for LowSVS */
+	735000,
+	895000,
+	1050000,
 };
 
 /* Defines mapping from retention fuse values to voltages in microvolts */
